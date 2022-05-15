b8296177aa6c6b252a10d5ab44cbd0b03c5f85a2
2eeYunHo <mmm01230@gmail.com>
2022-05-16 03:00:33 +0900

    과제완료

--- sql-practices/연습문제/sql-practices.sql
+++ sql-practices/연습문제/sql-practices.sql
@@ -30,8 +30,8 @@ select first_name, hire_date, date_add(hire_date, interval(5) year )
    from employees;
    
 -- cast
-select '12345' + '10',cast('12345' as int)+10, concat('1234','10');
-select date_format('22-10-10','%Y년 %m월 %d일'), date_format(cast('2022-10-10' as date), '%Y년 %m월 %d일');
+select '12345' + '10', cast('12345' as int) + 10, concat('12345', '10');
+select date_format('2022-10-10', '%Y년 %m월 %d일'), date_format(cast('2022-10-10' as date), '%Y년 %m월 %d일');
 select cast(cast(1-2 as unsigned) as signed);
 -- mysql typep // 아래 두개는 다름varchar는 공간이 남으면 남는공간을 줄여줌 효율적이나 대신에 속도가 떨어짐 
 -- varchar(10), char(10), text, CLOB(character Large Object)
